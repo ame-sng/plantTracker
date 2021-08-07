d83f8616a8acbda3fab6086f43e74707a83d71d2
“ame-sng” <ameliasnghy@gmail.com>
2021-08-07 18:58:28 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+// =======================================
+//              DEPENDENCIES
+// =======================================
+const express = require("express"); 
+const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const cors = require("cors");
+
+// =======================================
+//              CONFIGURATIONS
+// =======================================
+require("dotenv").config();
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT;
+
+// =======================================
+//         BODY PARSER/MIDDLEWARE
+// =======================================
+app.use(cors());
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.json()); //parse JSON back and forth
+app.use(express.static("public"))
+app.use(express.static("./client/build"));
+
+
+// =======================================
+//             MONGOOSE CONNECTION
+// =======================================
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
+
+//* =======================================
+//*         AUTHENTICATION
+//* =======================================
+
+
+
+
+
+// =======================================
+//         CONTROLLERS/ROUTES
+// =======================================
+const mockController = require("./controllers/controller");
+app.use("/v1/controller", mockController);
+
+//* allow for pathing on deployment e.g. Heroku
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
+
+// =======================================
+//              LISTENER
+// =======================================
+app.listen(PORT, () => {
+    console.log("Listening on the port", PORT);
+  }); 
