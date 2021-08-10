2450739f72f7fccd1588d3689b0b89bb234500cb
“ame-sng” <ameliasnghy@gmail.com>
2021-08-10 10:38:55 +0800

    tried eslint

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 // =======================================
 //              DEPENDENCIES
 // =======================================
-const express = require("express"); 
+const express = require("express");
+
 const app = express();
 const mongoose = require("mongoose");
 const session = require("express-session");
 const cors = require("cors");
-const path = require('path');
-
+const path = require("path");
 
 // =======================================
 //              CONFIGURATIONS
 // =======================================
 require("dotenv").config();
-const MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
 // =======================================
@@ -26,20 +27,16 @@ app.use(
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
-  })
+  }),
 );
-app.use(express.json()); //parse JSON back and forth
-app.use(express.static("public"))
+app.use(express.json()); // parse JSON back and forth
+app.use(express.static("public"));
 app.use(express.static("./client/build"));
-
-
 
 // =======================================
 //             MONGOOSE CONNECTION
 // =======================================
-mongoose.connection.on("error", (err) =>
-  console.log(err.message + " is Mongod not running?")
-);
+mongoose.connection.on("error", (err) => console.log(`${err.message} is Mongod not running?`));
 mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
 
 mongoose.connect(MONGODB_URI, {
@@ -52,15 +49,15 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongoose...");
 });
 
-
-
 // =======================================
 //         CONTROLLERS/ROUTES
 // =======================================
-const plantsController = require("./controllers/plants-controller.js");
+const plantsController = require("./controllers/plants-controller");
+
 app.use("/v1/plants", plantsController);
 
-const usersController = require("./controllers/users-controller.js");
+const usersController = require("./controllers/users-controller");
+
 app.use("/v1/users", usersController);
 
 // =======================================
@@ -74,5 +71,5 @@ app.get("/*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log("Listening on the port", PORT);
-  }); 
+  console.log("Listening on the port", PORT);
+});
